6bb7b873e64d19ffcf9ae0c4a379123ad1ef1f6c
Ritesh Sangwan <ritesh.sangwan@dineout.co.in>
2018-02-21 22:15:57 +0530

    maven project init and check null for terms

---
+++ src/main/java/com/github/saaay71/solr/VectorQuery.java
@@ -0,0 +1,58 @@
+package com.github.saaay71.solr;
+
+import org.apache.lucene.index.LeafReaderContext;
+import org.apache.lucene.search.ConstantScoreScorer;
+import org.apache.lucene.search.ConstantScoreWeight;
+import org.apache.lucene.search.DocIdSetIterator;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.Scorer;
+import org.apache.lucene.search.Weight;
+
+import java.io.IOException;
+
+public class VectorQuery extends Query {
+    String queryStr = "";
+    Query q;
+
+    public VectorQuery(Query subQuery) {
+        this.q = subQuery;
+    }
+
+    public void setQueryString(String queryString) {
+        this.queryStr = queryString;
+    }
+
+    @Override
+    public Weight createWeight(IndexSearcher searcher, boolean needsScores) throws IOException {
+        Weight w;
+        if (q == null) {
+            w = new ConstantScoreWeight(this) {
+                @Override
+                public Scorer scorer(LeafReaderContext context) throws IOException {
+                    return new ConstantScoreScorer(this, score(), DocIdSetIterator.all(context.reader().maxDoc()));
+                }
+            };
+        } else {
+            w = searcher.createWeight(q, needsScores);
+        }
+        return w;
+    }
+
+    @Override
+    public String toString(String field) {
+        return queryStr;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return sameClassAs(other) &&
+                queryStr.equals(other.toString());
+    }
+
+    @Override
+    public int hashCode() {
+        return classHash() ^ queryStr.hashCode();
+    }
+
+}
